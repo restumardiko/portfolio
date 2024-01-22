25c9b7d7544c6f7e5c30f1b941a12174098fd3e9
restumardiko <mardikorestu@gmail.com>
2024-01-22 12:25:02 +0700

    first brick for transition

---
+++ script.js
@@ -0,0 +1,4 @@
+  document.addEventListener("DOMContentLoaded", function () {
+        var slideElement = document.getElementById("slideElement");
+        slideElement.classList.add("active");
+      });
