3c2b39684265ddc6b676899aacaf7a43cd9edff4
restumardiko <mardikorestu@gmail.com>
2024-02-06 09:14:08 +0700

    on nav translation progres

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
         var slideElement = document.getElementById("slideElement");
         slideElement.classList.add("active");
       });
+
+// const navi = document.getElementsByClassName("navi")
+// const home=navi[0];
+// const about=navi[1];
+// const project=navi[2];
+// const contact=navi[3];
+// home.addEventListener("mouseover",()=>{
+//   console.log("gotcha")
+// })
+// home.addEventListener("mouseout",()=>{
+//   console.log("moveout")
+// })
+
+note 
