84a9687ecbfb319a5e221bf10c5bc12cd79896cb
restumardiko <mardikorestu@gmail.com>
2024-02-09 10:14:38 +0700

    add transition on home section

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var slideElement = document.getElementsByClassName("brick");
+const nav =document.querySelector('nav');
+const contenContainer =document.getElementById('content-container')
+const aside = document.querySelector('aside')
 document.addEventListener("DOMContentLoaded", function () {
+  nav.classList.add('active');
+  contenContainer.classList.add('active')
+  aside.classList.add('active');
+
+
       
       console.log(slideElement);
       for (var i = 0; i < 6; i++) {
