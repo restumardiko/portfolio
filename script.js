9d8db2242dd325210b41195aaa3b0d33f6e31a98
restumardiko <mardikorestu@gmail.com>
2024-04-28 13:02:15 +0700

    update navbar

--- script.js
+++ script.js
@@ -14,3 +14,49 @@ document.addEventListener("DOMContentLoaded", function () {
         slideElement[i].classList.add('active');}
     });
 
+
+    
+    let lastScrollTop = 0;
+    const navbar = document.getElementById("navbar");
+    let a = 0;
+    
+    window.addEventListener("scroll", () => {
+      let currentScroll = window.scrollY || document.documentElement.scrollTop;
+      
+    
+      if (currentScroll > lastScrollTop) {
+        if (a<=-50){
+          a=a-0;
+        }else {
+          a=a-5;
+        }
+   
+     
+     
+        
+        // Scroll down
+        console.log(a)
+        navbar.style.transform = `translateY(${a}px)`;
+
+       
+      
+      } else {
+        if (a>=0){
+          a=0
+
+        }
+        else {
+          a=a+5
+
+        }
+        // Scroll up
+
+    
+        console.log(a);
+        navbar.style.transform = `translateY(${a}px)`;
+
+
+      }
+      
+      lastScrollTop = currentScroll;
+    });
